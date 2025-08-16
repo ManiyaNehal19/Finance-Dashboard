eb05a75e68e77f7f62f2bb03bcd84fb9c7628d14
ManiyaNehal19 <maniyanehal@gmail.com>
2025-08-16 12:57:16 +0500

    process added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+/* eslint-env node */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/Finance-Dashboard",
+  base: process.env.VITE_BASE_PATH || "/Finance-Dashboard",
 })
